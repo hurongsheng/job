5fd2b5bc7fa8e78191ee79c3dbb23a96071dc650
海螺 <hurongsheng@innotechx.com>
2021-02-03 15:22:02 +0800

    数据库相关

--- try/compare_struct.go
+++ try/compare_struct.go
@@ -5,18 +5,23 @@ import (
 )
 
 type s struct {
-	Key string
+	Key  string
+	Key2 int
 }
 
 func main() {
 	CompareStruct()
 }
 func CompareStruct() {
+	int1 := 6
+	int2 := 6
 	s1 := s{
-		Key: "1",
+		Key:  "1",
+		Key2: int1,
 	}
 	s2 := s{
-		Key: "1",
+		Key:  "1",
+		Key2: int2,
 	}
 	fmt.Printf("%+v == %+v? %v\n", s1, s2, s1 == s2)
 	fmt.Printf("%+v == %+v? %v\n", s1, s2, &s1 == &s2)
